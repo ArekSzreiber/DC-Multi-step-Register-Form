5fa70e48b1c538b38ccbfc75261c315aad8cfb18
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2024-02-11 17:31:36 +0100

    add empty components and empty classes

---
+++ src/app/containers/stepper/stepper.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stepper',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      stepper works!
+    </p>
+  `,
+  styles: ``
+})
+export class StepperComponent {
+
+}
